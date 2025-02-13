d1caed4b2532fe1df2211e8f2de2f48623930f7c
Mats Hansen <math@hiof.no>
2025-02-13 01:25:47 +0100

    Fixed get all public messages

--- WebApp/src/models/Message.php
+++ WebApp/src/models/Message.php
@@ -184,7 +184,7 @@ class Message
     public function getPublicMessages()
     {
         try {
-            $sql = "SELECT id AS message_id, content, created_at FROM messages WHERE is_public = 1";
+            $sql = "SELECT id AS message_id, content, created_at FROM messages";
             $stmt = $this->pdo->query($sql);
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $e) {
