a2593063b47200adae0e5a25a2b86fa2a3d30174
snilseen <nilzen@live.com>
2025-02-17 12:50:48 +0100

    akjn
    1221

--- WebApp/vendor/composer/installed.php
+++ WebApp/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'snils/web-app',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '480cf63b6f25b7b94c322dbaa895822cc0ad815e',
+        'reference' => '914e5fa381e3cde486512f9cf536b7c3eb89f03e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'snils/web-app' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '480cf63b6f25b7b94c322dbaa895822cc0ad815e',
+            'reference' => '914e5fa381e3cde486512f9cf536b7c3eb89f03e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
