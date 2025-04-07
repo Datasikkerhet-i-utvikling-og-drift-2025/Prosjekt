720d05fec067a7e005efdd247923979393032f4f
snilseen <nilzen@live.com>
2025-04-07 19:59:55 +0200

    Fikset routing til guests
    Får routet til dashborded til guests nå hvertfall.

--- WebApp/src/views/guests/dashboard.php
+++ WebApp/src/views/guests/dashboard.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
-require_once __DIR__ . '/../../config/DatabaseManager.php';
+require_once __DIR__ . '/../../managers/DatabaseManager.php';
 require_once __DIR__ . '/../partials/header.php';
 require_once __DIR__ . '/../partials/navbar.php';
 
+
+use managers\DatabaseManager;
 // Sanitize output
 function sanitize($value) {
     return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
-$pdo = (new service\DatabaseService())->pdo;
+$pdo = new DatabaseManager();
 $pin = $_POST['pin'] ?? null;
 $authorized = false;
 $course = null;
